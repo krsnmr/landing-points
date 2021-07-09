830703f3071bf6fb12d37544f6fe6868b2660141
Shkutov KE <ske@npobaltros.ru>
2021-07-09 13:47:08 +0300

    Показ текущих высоты, скорости и штампа для определения положения

--- scripts/currPosition.js
+++ scripts/currPosition.js
@@ -1,65 +1,73 @@
-(function () {
-  var current_position; // маркер с определенной координатой
-  var current_accuracy; // область с точностью с которой определена координата
+(function() {
+    var current_position; // маркер с определенной координатой
+    var current_accuracy; // область с точностью с которой определена координата
 
-  function onLocationFound(e) {
-    // удалить старые объекты... if position defined, then remove the existing position marker and accuracy circle from the map
-    if (current_position) {
-      map.removeLayer(current_position);
-      map.removeLayer(current_accuracy);
+    function onLocationFound(e) {
+        // удалить старые объекты... if position defined, then remove the existing position marker and accuracy circle from the map
+        if (current_position) {
+            map.removeLayer(current_position);
+            map.removeLayer(current_accuracy);
+        }
+
+        var radius = Math.round(e.accuracy / 2);
+
+        //console.log('высота', e.altitude);
+        //console.log('скорость', e.speed);
+        //console.log('timestamp', e.timestamp);
+
+        document.getElementById('s1').style.color = 'red';
+        document.getElementById('s2').innerHTML =
+            "высота:" + e.altitude +
+            "; скорость:" + e.speed +
+            "; штамп:" + e.timestamp;
+        // сделать через 1 белым
+        window.setTimeout(
+            function() {
+                document.getElementById('s1').style.color = 'white';
+            },
+            500);
+
+        current_position = L.marker(e.latlng)
+            .addTo(map)
+            //.bindPopup("Устройство определило что вы находитесь внутри окружности на карте радиусом " + radius + " метров от этой точки")
+            //.openPopup();
+
+        current_accuracy = L.circle(e.latlng, radius).addTo(map);
     }
 
-    var radius = Math.round(e.accuracy / 2);
+    function onLocationError(e) {
+        alert(e.message);
+    }
 
-    document.getElementById('s1').style.color = 'red';
-    // сделать через 1 белым
-    window.setTimeout(
-      function () {
-        document.getElementById('s1').style.color = 'white';
-      },
-      500);
+    var pos = [59.851606, 30.196719];
+    var map = L.map("map").setView(pos, 14); //
+    map.on("locationfound", onLocationFound);
+    map.on("locationerror", onLocationError);
 
-    current_position = L.marker(e.latlng)
-      .addTo(map)
-    //.bindPopup("Устройство определило что вы находитесь внутри окружности на карте радиусом " + radius + " метров от этой точки")
-    //.openPopup();
+    // !!!!!! запустить переодическое определение координаты !!!!
+    map.locate({
+        setView: false,
+        maxZoom: 16,
+        watch: true,
+        timeout: 10000, // 10сек в случае ошибки
+    });
 
-    current_accuracy = L.circle(e.latlng, radius).addTo(map);
-  }
+    // после загрузки инициировать карту
+    window.onload = function() {
 
-  function onLocationError(e) {
-    alert(e.message);
-  }
+    };
 
-  var pos = [59.851606, 30.196719];
-  var map = L.map("map").setView(pos, 14); //
-  map.on("locationfound", onLocationFound);
-  map.on("locationerror", onLocationError);
-
-  // !!!!!! запустить переодическое определение координаты !!!!
-  map.locate({
-    setView: false,
-    maxZoom: 16,
-    watch: true,
-    timeout: 10000, // 10сек в случае ошибки
-  });
-
-  // после загрузки инициировать карту
-  window.onload = function () {
-   
-  };
-
-  var initMap = function () {
-    OSM_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-    L.tileLayer(OSM_URL, {
-      maxZoom: 18,
-      attribution: "Положение на карте",
-      id: "mapbox.streets",
-    }).addTo(map);
-  };
+    var initMap = function() {
+        OSM_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+        L.tileLayer(OSM_URL, {
+            maxZoom: 18,
+            attribution: "Положение на карте",
+            id: "mapbox.streets",
+        }).addTo(map);
+    };
 
 
 
-  initMap();
-  
+    initMap();
+
 })();
