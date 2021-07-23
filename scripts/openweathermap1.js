51391346f8bfc86010ba23c3125b494198e90074
Konst Shkutov <kstshktv@yahoo.com>
2021-07-23 23:40:11 +0300

    Добавил информацию с сервиса погоды

---
+++ scripts/openweathermap1.js
@@ -0,0 +1,28 @@
+var owm1 = (function($){
+
+    var href0 = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2abe21ecc1e023a3e634fc34f9cc1ff0";
+
+    var href = "http://api.openweathermap.org/data/2.5/weather?id=866055&lang=ru&units=metric&appid=2abe21ecc1e023a3e634fc34f9cc1ff0";
+
+
+    $(function(){
+
+        $.get(href, function(data){
+                console.log('напр ветра - ', data.wind.deg);
+                    //&units=metric&lang=ru
+                $('#demo1').text(data.wind.deg);
+                $('#demo2').text(data.wind.speed);
+                $('#demo3').text(data.wind.gust);
+                $('#demo4').text(data.weather[0].description);
+                $('#demo5').text(data.main.temp);
+                var iconurl = "http://openweathermap.org/img/w/" +  data.weather[0].icon + ".png";
+
+                $('#wicon').attr('src', iconurl);
+        });
+
+
+        //console.log(11111);
+
+    });
+
+})(jQuery);
