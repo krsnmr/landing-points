e40e4a6e8e00a3022dbe52b0b335c44aaa5aaee7
Konst Shkutov <kstshktv@yahoo.com>
2021-08-18 21:10:10 +0300

    Компонент со стрелкой с направлением

--- scripts/mapTest1.js
+++ scripts/mapTest1.js
@@ -9,8 +9,9 @@ var owm1 = (function ($, ko, L) {
     var map;
 
     var initMap = function () {
-        map = L.map("map");
-        L.tileLayer(OSM_URL, {   maxZoom: 18, attribution: "Путилово аэродром", id: "map" }).addTo(map);
+        map = L.map('map').setView([30.201479, 120.155908], 13);
+        L.tileLayer(OSM_URL, { maxZoom: 18, attribution: "Путилово аэродром", id: "map" }).addTo(map);
+
     };
 
     var iconRedArrow = L.icon({
@@ -19,20 +20,73 @@ var owm1 = (function ($, ko, L) {
         iconAnchor: [8, 24],
         popupAnchor: [-3, -76],
     });
-    
+
 
     $(function () {
-       
+
         initMap();
 
-        var layerGroup1 = L.layerGroup().addTo(map);
+        //var layerGroup1 = L.layerGroup().addTo(map);
 
-        var windMarker = L.marker(currentLandingPoint, { icon: iconRedArrow });
-        windMarker.addTo(layerGroup1);
+        //var windMarker = L.marker(currentLandingPoint, { icon: iconRedArrow });
+        //windMarker.addTo(layerGroup1);
 
-        map.setView(currentLandingPoint, 16);
-       
+        //map.setView(currentLandingPoint, 16);
+
+
+        initMarkers();
+
+
     });
+
+
+    // your data
+    var list = [
+        { "lon": "120.15261", "lat": "30.182835" },
+        { "lon": "120.152431666667", "lat": "30.1828733333333" },
+        { "lon": "120.15239", "lat": "30.182755" },
+        { "lon": "120.152303333333", "lat": "30.1826566666667" }
+    ];
+
+    var initMarkers = function () {
+
+        // iterate your data       
+        for (var i in list) {
+            var img = new Image();
+            img.src = './scripts/images/arrow.png';
+
+            var options = {
+                label: 'your label',
+                labelFlag: false,
+                labelColor: 'black',
+                img: img
+            };
+
+            console.log(list[i].lat + ' - ' + list[i].lon);
+
+            var lat = Number(list[i].lat);// + offSiteY;
+            var lon = Number(list[i].lon);// - offSiteX;
+            var latlng = { lat: lat, lon: lon };            
+
+            // use angeMaker plugin
+            var angleMarker = L.angleMarker(latlng, options);
+            var angle = 0;
+
+            if (i > 0) {
+                var previousLatLng = { lat: list[i - 1].lat, lon: list[i - 1].lon };
+                var nextLanLng = { lat: list[i].lat, lon: list[i].lon };
+                // get angele between A(previousPoint) and B(nextPoint)
+                angle = angleMarker.getAngle(previousLatLng, nextLanLng);
+            }
+            // set angele A -> B
+            angleMarker.setHeading(angle);
+            map.addLayer(angleMarker);
+        }
+
+
+    }
+
+
 
 
 })(jQuery, ko, L);
